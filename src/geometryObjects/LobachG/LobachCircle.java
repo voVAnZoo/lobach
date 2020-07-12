76b981dc9cc0c0d306a71f195999996df9eb024c
north-cactus <nikita.fedorov.00@bk.ru>
2020-07-12 03:33:17 +0300

    Added LobachCircle toEuclidObject method

--- src/geometryObjects/LobachG/LobachCircle.java
+++ src/geometryObjects/LobachG/LobachCircle.java
@@ -1,23 +1,30 @@
 package geometryObjects.LobachG;
 
+import core.Data;
+import immersiveMath.Cnumbers;
 import geometryObjects.EuclidObject;
+import geometryObjects.EuclidG.Circle;
 import geometryObjects.LobachObject;
 
 public class LobachCircle extends LobachObject {
+    Cnumbers center;
     double r;
-    double center;
+    static double R;
 
-    double w1;
-    double w2;
-
-    public LobachCircle(double r, double center){
+    public LobachCircle(Cnumbers center, double r ){
         this.r = r;
         this.center = center;
+	R = Data.R;
     }
-
 
     @Override
     public EuclidObject toEuclidObject() {
-        return null;
+	double c = center.abs();
+	double A = (c - R)/(c + R);
+	double w1 = R * (1 + A * Math.exp(r))/(1 - A * Math.exp(r));
+	double w2 = R * (1 - A * Math.exp(-r))/(1 + A * Math.exp(-r));
+	Cnumbers euclidCenter = center.scale((w1 + w2) / (2 * center.abs()));
+	double euclidRadius   = Math.abs((w2 - w1) / 2);
+        return new Circle(euclidCenter, euclidRadius);
     }
 }
