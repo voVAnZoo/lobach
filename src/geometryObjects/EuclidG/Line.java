6ba370a9d9138c18c884d2d0c3e498b3c66c5763
north-cactus <nikita.fedorov.00@bk.ru>
2020-07-12 04:40:50 +0300

    Added equals method for Line class.

--- src/geometryObjects/EuclidG/Line.java
+++ src/geometryObjects/EuclidG/Line.java
@@ -14,5 +14,17 @@ public class Line extends EuclidObject {
 		point = new Cnumbers(0);
 		normal = norm;
 	}
+	public Cnumbers getNormal() {return normal;}
+	public Cnumbers getPoint()  {return point;}
+	public double euclideanDistance(Cnumbers x){
+		return normal.scalarProduct(x) - normal.scalarProduct(point);
+	}
 
+	public boolean equals(Object x) {
+		if (x == null) return false;
+		if (this.getClass() != x.getClass()) return false;
+		Line that = (Line) x;
+		if (this.normal != that.getNormal()) return false;
+		return euclideanDistance(that.getPoint()) == 0;
+	}
 }
