5c2b8e43e232dd4952dc22b40fac25bd8de67804
north-cactus <nikita.fedorov.00@bk.ru>
2020-07-13 08:47:20 +0300

    Finished intersection method.

--- src/geometryObjects/GeometryObject.java
+++ src/geometryObjects/GeometryObject.java
@@ -59,43 +59,55 @@ public class GeometryObject {
     public static List<Point> intersection(Circle a, Circle b){
         List<Point> out = new ArrayList<Point>();
 
-
-        if(a.getCenter().minus(b.getCenter()).abs() == a.getR() + b.getR()){
-
+        // let the first circl be the bigest one
+        if (a.getR() < b.getR()){
+            Circle tmp = a;
+            a = b;
+            b = tmp;
         }
-
-        if((a.getR() >= b.getR()) && (a.getCenter().minus(b.getCenter()).abs() + b.getR() == a.getR())){
-
-        }
-
-        if((b.getR() >= a.getR()) && (a.getCenter().minus(b.getCenter()).abs() + a.getR() == b.getR())){
-
-        }
-
-        if(a.getCenter().minus(b.getCenter()).abs() < a.getR() + b.getR()) {
-            double A = (b.getCenter().re() - a.getCenter().re());
-            double B = (b.getCenter().im() - a.getCenter().im());
-            double C = (A * A) + (B * B) + (a.getR() * a.getR()) - (b.getR() * b.getR());
-            A *= (-2);
-            B *= (-2);
-
-            double a2 = (A * A) + (B * B);
-            double b2 = (-2) * C * B;
-            double c2 = (C * C) - (A * A * a.getR() * a.getR());
-
-            double d = (b2 * b2) - (4 * a2 * c2);
-
-            if (d == 0) {
-                double y = -b2 / (2 * a2);
-                if (A != 0) {
-                    double x = (C - (B * y)) / A;
-                    //я хочу спать, О Великая Богиня Иштар, молю,
-                    // пусть это код кто-нибудь допишет вместо меня
-                    // зарание спасибо! =)
-                }
+        // r1 > r2
+        double r1 = a.getR();
+        double r2 = b.getR();
+        Cnumbers o1 = a.getCenter();
+        Cnumbers o2 = b.getCenter();
+        double o1o2 = o1.minus(o2).abs();
+        // the o2 lies inside the first circle 
+        if (o1o2 < r1){
+            if (o1o2 + r2 < r1) {
+                return out;
+            }
+            // contact case
+            if (o1o2 + r2 == r1) {
+                Cnumbers contactP = o1.plus(o2.minus(o1).scale(r1/o1o2));
+                out.add(new Point(contactP));
+                return out;
             }
         }
+        else {
+            if (o1o2 > r1 + r2) {
+                return out;
+            }
+            // contact case
+            if (o1o2 == r1 + r2) {
+                Cnumbers contactP = o1.plus(o2.minus(o1).scale(r1/o1o2));
+                out.add(new Point(contactP));
+                return out;
+            }
+        }
+        if (o1o2 == 0) {
+            return out;
+        }
 
+        // q is the point of intersection of radical axis and o1o2
+        // oq = (o1o2^2 + r1^2 - r2^2) / (2 o1o2)
+        double o1q = (o1o2*o1o2 + r1*r1 - r2*r2) / (2 * o1o2);
+        // h is a heigh of intersection points on the radical axis
+        double h = Math.sqrt(r1*r1 - o1q*o1q);
+        // the points of intersection are o1 + (o2-o1)/o1o2 (o1q +- i h)
+        Cnumbers p1 = o1.plus(o2.minus(o1).scale(1/o1o2).multiply(new Cnumbers(o1q, h)));
+        out.add(new Point(p1));
+        Cnumbers p2 = o1.plus(o2.minus(o1).scale(1/o1o2).multiply(new Cnumbers(o1q, -h)));
+        out.add(new Point(p2));
         return out;
     }
 
@@ -162,11 +174,9 @@ public class GeometryObject {
     public static List<Point> intersection(Point a, Line b){
         List<Point> out = new ArrayList<Point>();
 
-        Cnumbers tan = b.getNormal().multiply(Cnumbers.i());
-        if(a.getP().minus(b.getPoint()).phase() == tan.phase()){
+        if(a.getP().minus(b.getPoint()).scalarProduct(b.getNormal()) == 0){
             out.add(a);
         }
-
         return out;
     }
 }
